6106b222f958390ebfa5c1569d26a64e042682ef
mray1995 <sleepingbag5927@gmail.com>
2021-08-04 17:50:14 -0500

    New exercise works

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
@@ -8,14 +9,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'))
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+app.get("/exercise", (req, res) => {
+  
+  res.sendFile(path.join(__dirname,'/public/exercise.html'))
+});
 
 
 app.listen(PORT, () => {
